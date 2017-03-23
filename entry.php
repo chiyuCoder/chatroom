65ca072bb3adaed01ac4a65872d7ea9e22f5193c
sin-iti <shinichilyers@gmail.com>
2017-03-23 13:35:03 +0800

    pre-v02

--- entry.php
+++ entry.php
@@ -5,11 +5,8 @@
 	require_once 'workerman/Autoloader.php';
 	// $myHttpMethod = new newhttp();
 	
-<<<<<<< HEAD
+
 	$myHTTP = new WebServer('http://192.168.199.189:3000');
-=======
-	$myHTTP = new WebServer('http://127.0.0.1:3000');
->>>>>>> 0d839c2704fd37014d36e68ebec9752cfa8daa59
 	$myHTTP->count = 1;
 	$myHTTP->addRoot('www.your_domain.com', __DIR__.'/app');
 	
